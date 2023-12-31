a01fb118fa021afe5d0c6e23ab6f5a59bab0ecbe
bkRyusim <bkryusim@gmail.com>
2023-12-31 18:48:13 +0900

    feat: loginpage with chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {LoginPage} from "./login/LoginPage";
+import {Box, Button, Flex, Heading, HStack, Text} from "@chakra-ui/react";
+import {Link as RouterLink} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Flex flexDirection={"column"} align={"center"}>
+            <Flex
+                width={"100%"}
+                height={"60px"}
+                flexDirection={"column"}
+                borderBottom={"1px solid"}
+                borderBottomColor={"#f5f5f5"}
+                alignItems={"center"}
+            >
+                <Flex alignItems={"center"} height={"100%"} width={960} justifyContent={"space-between"}>
+                    <Flex>
+                        <Heading margin={0} padding={0} color={"black"}>Reflog</Heading>
+                        <HStack gap={"20px"} ml={"40px"}>
+                            <Text>회고 작성하기</Text>
+                            <Text>팀 둘러보기</Text>
+                        </HStack>
+                    </Flex>
+                    <RouterLink to={"/login"}><Button variant={"outline"}>로그인</Button></RouterLink>
+                </Flex>
+            </Flex>
+
+            <Box width={960} mt={"60px"}>
+                <LoginPage/>
+            </Box>
+        </Flex>
+    );
 }
 
 export default App;
---
+++ src/login/LoginPage.tsx
@@ -0,0 +1,37 @@
+import {
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Link as ChakraLink,
+    Text,
+    VStack
+} from "@chakra-ui/react";
+import {Link as RouterLink} from "react-router-dom";
+
+export const LoginPage = () => {
+    return (
+        <Flex flexDirection={"column"} align={"center"} gap={"30px"}>
+            <Heading>로그인</Heading>
+                <VStack width={300} gap={"20px"}>
+                    <FormControl>
+                        <FormLabel>이메일</FormLabel>
+                        <Input type={"email"}/>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>비밀번호</FormLabel>
+                        <Input type={"password"}/>
+                    </FormControl>
+                    <Button colorScheme={"teal"} width={"100%"}>로그인</Button>
+                </VStack>
+                <VStack>
+                    <Text>아직 Reflog의 회원이 아니신가요? <ChakraLink as={RouterLink} to={"/"} color={"teal.500"}>회원가입</ChakraLink>
+                    </Text>
+                    <Text>아이디, 비밀번호가 기억나지 않는다면, <ChakraLink as={RouterLink} to={"/"} color={"teal.500"}>아이디/비밀번호 찾기</ChakraLink>
+                    </Text>
+                </VStack>
+        </Flex>
+    )
+}
